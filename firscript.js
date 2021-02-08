cf6ad219c635d4eec6e2ac3388b17ab496a8a41f
chetan639 <gchetan2000@gmail.com>
2021-02-08 22:42:47 +0530

    Add homepage with its css. And responsiveness added to the navbar

--- firscript.js
+++ firscript.js
@@ -1,15 +1,28 @@
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
     }
-}
+  }
+};
+
+const navSlide = function () {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", function () {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
