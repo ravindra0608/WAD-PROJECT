c3625b3b596168e026c9308ec2284f9817e8d86c
chetan639 <gchetan2000@gmail.com>
2021-02-20 19:46:14 +0530

    Add a background image to homepage Add twitter iframe and its working fully Put the JS content of the homepage in a new different file and added some content to that also

--- firscript.js
+++ firscript.js
@@ -13,16 +13,3 @@ window.onclick = function (event) {
     }
   }
 };
-
-const navSlide = function () {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-
-  burger.addEventListener("click", function () {
-    nav.classList.toggle("nav-active");
-    burger.classList.toggle("toggle");
-  });
-};
-
-navSlide();
