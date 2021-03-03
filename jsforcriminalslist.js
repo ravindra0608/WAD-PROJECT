1892edc0be0775cb1b36ee064a73e4f3aa116e47
chetan639 <gchetan2000@gmail.com>
2021-03-03 23:40:49 +0530

    Made the basic model of Criminals List

---
+++ jsforcriminalslist.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-close").addEventListener("click", function () {
+  document.getElementById("side-menu").style.width = "0";
+});
+
+document.getElementById("side-bur").addEventListener("click", function () {
+  document.getElementById("side-menu").style.width = "250px";
+});
