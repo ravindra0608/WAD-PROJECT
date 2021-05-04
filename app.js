d6c1d1af09474ade8bb41692987b39e91c7e23ee
chetan639 <gchetan2000@gmail.com>
2021-05-04 17:48:46 +0530

    Made the Criminals list work

--- app.js
+++ app.js
@@ -8,14 +8,8 @@ const path = require("path");
 // require("dotenv/config");
 
 const app = express();
-app.use(
-  express.json({
-    limit: "50mb",
-  })
-);
-app.use(
-  express.urlencoded({ extended: true, limit: "50mb", parameterLimit: 100000 })
-);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
@@ -120,6 +114,15 @@ app.post("/delete", function (req, res) {
 });
 
 //Criminals list
+
+app.get("/criminalslist", function (req, res) {
+  let wanted = [];
+  let arrested = [];
+  let ontrail = [];
+  Criminal.find({}, function (err, criminalsList) {
+    res.render("criminalslist", { criminalsList: criminalsList });
+  });
+});
 
 app.get("/postcriminalslist", function (req, res) {
   Criminal.find({}, function (err, criminalsList) {
