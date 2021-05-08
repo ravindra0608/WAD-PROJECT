eb13b189789d42d1566404e8a8232b2f99ed530e
Ravindra Tholuchuru <tholuchururavindra@gmail.com>
2021-05-08 21:09:14 +0530

    Removed conflict in postcriminals

--- app.js
+++ app.js
@@ -229,43 +229,43 @@ app.get("/logout", function(req, res) {
 });
 
 // faq page routes
-app.get('/faq', function(req, res){
-  return res.render('faq');
+app.get('/faq', function(req, res) {
+    return res.render('faq');
 });
 
 // gallery page
-app.get("/gallery",async function (req, res) {
-  let events1 = await Events.find({});
-  app.locals.policeEvents = events1;
-  return res.render("Gallery");
+app.get("/gallery", async function(req, res) {
+    let events1 = await Events.find({});
+    app.locals.policeEvents = events1;
+    return res.render("Gallery");
 });
 
 // contact us page
-app.get("/contactus",async function (req, res) {
-  let policeDetails1 = await PoliceDetails.find({});
-  return res.render("Contact-us",{
-    policedet: policeDetails1
-  });
+app.get("/contactus", async function(req, res) {
+    let policeDetails1 = await PoliceDetails.find({});
+    return res.render("Contact-us", {
+        policedet: policeDetails1
+    });
 });
 
 // contact-us police side
-app.get("/pcontactus",async function (req, res) {
-  let policeDetails1 = await PoliceDetails.find({});
-  return res.render("contactus_police",{
-    policedet: policeDetails1
-  });
+app.get("/pcontactus", async function(req, res) {
+    let policeDetails1 = await PoliceDetails.find({});
+    return res.render("contactus_police", {
+        policedet: policeDetails1
+    });
 });
 
 // contactus adding
-app.post('/contactus/add',function (req, res) {
-  PoliceDetails.create(req.body);
-  return res.redirect('back');
+app.post('/contactus/add', function(req, res) {
+    PoliceDetails.create(req.body);
+    return res.redirect('back');
 });
 
-app.get('/contactus/delete/:stationId',async function (req, res) {
-  let station = await PoliceDetails.findById(req.params.stationId);
-  station.remove();
-  return res.redirect('back');
+app.get('/contactus/delete/:stationId', async function(req, res) {
+    let station = await PoliceDetails.findById(req.params.stationId);
+    station.remove();
+    return res.redirect('back');
 })
 
 // police side gallery page
@@ -279,33 +279,33 @@ app.get("/gallerypolice", async function(req, res) {
 });
 // adding an event to the database
 app.post(
-  "/gallerypolice/add-event",
-upload.single("tile"),
-async function (req, res) {
-    // console.log(req.body);
-    // console.log(__dirname);
-    let date1 = (req.body.date + "").slice(0, 15);
-    let event = await Events.findOne({
-      name: req.body.name,
-      date: req.body.date,
-    });
-    if (!event) {
-        if (req.file) {
-            Events.create({
-                name: req.body.name,
-                date: date1,
-                tile: "\\" + "uploads" + "\\" + req.file.filename,
-            });
+    "/gallerypolice/add-event",
+    upload.single("tile"),
+    async function(req, res) {
+        // console.log(req.body);
+        // console.log(__dirname);
+        let date1 = (req.body.date + "").slice(0, 15);
+        let event = await Events.findOne({
+            name: req.body.name,
+            date: req.body.date,
+        });
+        if (!event) {
+            if (req.file) {
+                Events.create({
+                    name: req.body.name,
+                    date: date1,
+                    tile: "\\" + "uploads" + "\\" + req.file.filename,
+                });
+            } else {
+                Events.create({
+                    name: req.body.name,
+                    date: req.body.date,
+                });
+            }
         } else {
-            Events.create({
-                name: req.body.name,
-                date: req.body.date,
-            });
+            console.log("you cannot created an event");
         }
-    } else {
-        console.log("you cannot created an event");
-    }
-    return res.redirect("back");
+        return res.redirect("back");
     }
 );
 
@@ -424,7 +424,7 @@ app.post("/postcriminalslist", upload.single("image"), function(req, res) {
         status: req.body.status,
         img: {
             data: fs.readFileSync(
-                path.join(__dirname + "/uploads/" + req.file.evidence)
+                path.join(__dirname + "/uploads/" + req.file.filename)
             ),
             contentType: "image/png",
         },
